7bba5c279b0d03c2fdbcdbaac83b8e27fc9645ce
Samuel <samueltan1897@gmail.com>
2019-12-15 15:12:04 -0500

    Adjusted time settings

--- wave_comparison/main.py
+++ wave_comparison/main.py
@@ -8,6 +8,7 @@ import numpy as np
 class SoundComparison:
 
     def __init__(self):
+        self.input_sound_start_snip = 0
         self.result = {"too_little_breaks": False, "too_many_breaks": False,
                        "short_breaks": [], "long_breaks": [],
                        "short_pronunciation": [], "long_pronunciation": []}
@@ -22,23 +23,24 @@ class SoundComparison:
         correct_data = self.normalize_audio_data_wave(correct_data)
         speaker_data_silence = self.calculate_silent_amplitude(speaker_data, speaker_rate)
         correct_data_silence = self.calculate_silent_amplitude(correct_data, correct_rate, 0)
-        speaker_data = self.remove_audio_wave_silence(speaker_data, speaker_rate)
+        #speaker_sound must be snipped of silence after correct sound file
         correct_data = self.remove_audio_wave_silence(correct_data, correct_rate, 0)
+        speaker_data = self.remove_audio_wave_silence(speaker_data, speaker_rate)
         speaker_silence = self.find_audio_chunk_breaks(speaker_data, speaker_rate, speaker_data_silence)
         correct_silence = self.find_audio_chunk_breaks(correct_data, correct_rate, correct_data_silence)
 
 
         # plot amplitude (or loudness) over time
-        # speaker_time = np.arange(0, len(speaker_data), 1) / speaker_rate
-        # correct_time = np.arange(0, len(correct_data), 1) / correct_rate
-        # plt.figure(1)
-        # plt.subplot(211)
-        # plt.plot(speaker_time, speaker_data, linewidth=0.1, alpha=1, color='#000000')
-        # plt.xlabel('Time (s)')
-        # plt.ylabel('Amplitude')
-        # plt.subplot(212)
-        # plt.plot(correct_time, correct_data, linewidth=0.1, alpha=1, color='#000000')
-        # plt.show()
+        speaker_time = np.arange(0, len(speaker_data), 1) / speaker_rate
+        correct_time = np.arange(0, len(correct_data), 1) / correct_rate
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(speaker_time, speaker_data, linewidth=0.1, alpha=1, color='#000000')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.subplot(212)
+        plt.plot(correct_time, correct_data, linewidth=0.1, alpha=1, color='#000000')
+        plt.show()
         self.check_sensibility_of_breaks(speaker_silence, correct_silence)
         return self.result
 
@@ -86,13 +88,13 @@ class SoundComparison:
                 if (speaker_end - speaker_start) > 2.2:
                     self.result["long_breaks"].append(speaker_breaks[i])
                 elif (speaker_break_time - correct_break_time) > 0.30:
-                    self.result["long_breaks"].append(speaker_breaks[i])
+                    self.result["long_breaks"].append((speaker_breaks[i][0] + self.input_sound_start_snip, speaker_breaks[i][1] + self.input_sound_start_snip))
                 elif (correct_break_time - speaker_break_time) > 0.30:
-                    self.result["short_breaks"].append(speaker_breaks[i])
+                    self.result["short_breaks"].append((speaker_breaks[i][0] + self.input_sound_start_snip, speaker_breaks[i][1] + self.input_sound_start_snip))
                 if (speaker_start - correct_start) > (last_time_difference + 0.5):
-                    self.result["long_pronunciation"].append((speaker_breaks[i-1][1], speaker_breaks[i][0]))
+                    self.result["long_pronunciation"].append((speaker_breaks[i-1][1] + self.input_sound_start_snip, speaker_breaks[i][0] + self.input_sound_start_snip))
                 elif (correct_start - speaker_start) > (last_time_difference + 0.5):
-                    self.result["short_pronunciation"].append((speaker_breaks[i-1][1], speaker_breaks[i][0]))
+                    self.result["short_pronunciation"].append((speaker_breaks[i-1][1] + self.input_sound_start_snip, speaker_breaks[i][0] + self.input_sound_start_snip))
                 last_time_difference = abs(correct_end - speaker_end)
 
     def remove_audio_wave_silence(self, audio_data, rate, min=None):
@@ -103,6 +105,7 @@ class SoundComparison:
             start_counter += 1
         while reversed_audio_data[end_counter] <= self.calculate_silent_amplitude(reversed_audio_data, rate, min):
             end_counter += 1
+        self.input_sound_start_snip = start_counter / rate
         return audio_data[start_counter: (len(audio_data) - end_counter)]
 
     def calculate_silent_amplitude(self, audio_data, rate, min=None):
